b57ac4e1250a9c4dd0ec6d974863ec4b0703dc8f
supersakana <zacwilliamson@icloud.com>
2022-12-26 11:26:52 -0600

    refactored js code and inputs are cleared after submission

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let myLibrary = [];
-let myList = document.querySelector('.book-list')
+let ul = document.querySelector('.book-list')
 let submitBtn = document.querySelector('.submit')
+let inputs = document.querySelectorAll('.form-input')
 
+// adds book to the library and appends on page
 submitBtn.addEventListener('click', function(){
     const newBook = new Book(
         document.getElementById('title').value,
@@ -10,11 +12,11 @@ submitBtn.addEventListener('click', function(){
         document.getElementById('read').value
     )
     addBookToLibrary(newBook)
-    listItem = document.createElement('li')
-    listItem.appendChild(document.createTextNode(`${newBook.info()}`))
-    myList.append(listItem);
+    appendBook(newBook)
+    clearInputs()
 })
 
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -25,16 +27,30 @@ function Book(title, author, pages, read) {
     }
 }
 
+// Adds book to library array
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+// appends book to the DOM as li
+function appendBook(book) {
+    li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${book.info()}`))
+    ul.append(li);
+}
+
+// clears form inputs after submission
+function clearInputs() {
+    inputs.forEach((input)=> {
+        input.value = ''
+    })
+}
+
+// displays each book in the library
 function displayLibrary(){
-    for(i = 0; i < myLibrary.length; i++){
-        listItem = document.createElement('li')
-        listItem.appendChild(document.createTextNode(`${myLibrary[i].info()}`))
-        myList.append(listItem);
-    }
+    myLibrary.forEach((book)=> {
+        appendBook(book)
+    })
 }
 
 const atl = new Book('Atlas Shrugged', 'Ayn Rand', 1000, 'not read')
