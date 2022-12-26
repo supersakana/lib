d3fb85d409753a30bc71cbd52ae6b068f6d0c19b
supersakana <zacwilliamson@icloud.com>
2022-12-26 08:35:51 -0600

    added basic book constructor and function for adding a new book to the library

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('im working')
+let myLibrary = [];
+let myList = document.querySelector('.book-list')
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
+}
+
+// addBookToLibrary('Atlas Shrugged', 'Ayn Rand', 1000, 'not read')
+// console.log(myLibrary[0].title)
