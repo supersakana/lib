b1cf5cedba28083c5c6cfb445f67613f4032ceec
supersakana <zacwilliamson@icloud.com>
2022-12-27 11:27:56 -0600

    refactored append book function

--- app.js
+++ app.js
@@ -27,34 +27,18 @@ function Book(title, author, pages, read) {
     }
 }
 
-// Adds book to library array
+// adds book to library array
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
 // appends book to the DOM as li along with delete button
 function appendBook(book) {
-    let li = document.createElement('li')
-    let p = document.createElement('p')
-    let deleteBtn = document.createElement('button')
-    let readBtn = document.createElement('input')
-    let btnContainer = document.createElement('div')
-
-    p.innerHTML = `${book.info()}`
-    deleteBtn.innerHTML = 'delete'
-    readBtn.type = 'checkbox'
-    if (book.read == true){
-        readBtn.checked = true
-
-    } else {
-        readBtn.checked = false
-    }
-    
-    li.id = `item-${myLibrary.indexOf(book)}`
-    li.classList.add('flex', 'items-center', 'justify-between')
-    deleteBtn.dataset.index = myLibrary.indexOf(book)
-    deleteBtn.classList.add('text-red-500', 'ml-3', 'delete-btn')
-
+    let li = newItem(book)
+    let p = document.createElement('p').innerHTML = `${book.info()}`
+    let deleteBtn = createDelete(book)
+    let readBtn = createBox(book)
+    let div = document.createElement('div')
 
     deleteBtn.addEventListener('click', function(){
         item = document.getElementById(`item-${deleteBtn.dataset.index}`)
@@ -63,9 +47,43 @@ function appendBook(book) {
 
     ul.append(li);
     li.append(p);
-    li.append(btnContainer);
-    btnContainer.append(readBtn)
-    btnContainer.append(deleteBtn)
+    li.append(div);
+    div.append(readBtn)
+    div.append(deleteBtn)
+}
+
+// create the list item for library
+function newItem(book){
+    item = document.createElement('li')
+    item.id = `item-${myLibrary.indexOf(book)}`
+    item.classList.add('flex', 'items-center', 'justify-between')
+
+    return item
+}
+
+// create the delete button
+function createDelete(book){
+    btn = document.createElement('button');
+    btn.innerHTML = 'delete'
+    btn.dataset.index = myLibrary.indexOf(book)
+    btn.classList.add('text-red-500', 'ml-3', 'delete-btn')
+
+    return btn
+}
+
+// creates the read checkbox
+function createBox(book){
+    box = document.createElement('input')
+    box.type = 'checkbox'
+
+    if (book.read == true){
+        box.checked = true
+
+    } else {
+        box.checked = false
+    }
+
+    return box
 }
 
 // clears form inputs after submission
@@ -82,8 +100,3 @@ function displayLibrary(){
         appendBook(book)
     })
 }
-
-const atl = new Book('Atlas Shrugged', 'Ayn Rand', 1000, false)
-
-addBookToLibrary(atl)
-displayLibrary()
